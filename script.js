7a2300d9533b39d252a73ea0111496c8f51a7d5b
KenGuuu <przemek.szumczyk@gmail.com>
2019-04-18 02:32:43 +0200

    Trying to make additional functions

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ setInterval(function GetTime()
     var today = new Date();
     var date = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
+    var dateTime = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear()+' '+time;
 
+    document.title=dateTime;
     document.getElementById("time").innerHTML = time;
     document.getElementById("date").innerHTML = date;
+
 }
 , 0);
 
+if(document.getElementById("countries").value=="UK")
+{
 setInterval(function Welcoming()
 {
     var d = new Date();
@@ -17,23 +21,25 @@ setInterval(function Welcoming()
     if(h<=12 && h>4)
     {
         document.getElementById("welcome").innerHTML = "Good Morning!";
-        return "Good Morning!";
     }
     else if(h>12 && h<18)
     {
         document.getElementById("welcome").innerHTML = "Good Afternoon!";
-        return "Good Afternoon!";
     }
     else if(h>=18 && h<21)
     {
         document.getElementById("welcome").innerHTML = "Good Evening";
-        return "Good Evening";
     }
     else
     {
         document.getElementById("welcome").innerHTML = "You should go to bed!";
-        return "You should go to bed!";
     }
 }
 , 0);
+}
+else
+{
+    document.getElementById("welcome").innerHTML = "Working";
+}
+
 
